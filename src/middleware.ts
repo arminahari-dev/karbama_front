5b7b98de96cf986548c1895e17ea8c1e643638ea
armin ahari <ahariarmin@gmail.com>
2025-08-08 21:38:09 +0330

    refactor: comment out middleware logic and related imports

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,76 +1,76 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
+// import type { NextRequest } from "next/server";
+// import { jwtVerify } from "jose";
 
-if (!process.env.ACCESS_TOKEN_SECRET_KEY) {
-  throw new Error("ACCESS_TOKEN_SECRET_KEY is not set!");
-}
+// if (!process.env.ACCESS_TOKEN_SECRET_KEY) {
+//   throw new Error("ACCESS_TOKEN_SECRET_KEY is not set!");
+// }
 
-const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET_KEY);
+// const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET_KEY);
 
-export async function middleware(request: NextRequest) {
-  console.log("Cookies keys:", [...request.cookies.keys()]);
+export async function middleware() {
+  // console.log("Cookies keys:", [...request.cookies.keys()]);
 
-  const getCookieValue = (key: string) => {
-    if (!key) return null;
-    const cookie = request.cookies.get(key);
-    if (!cookie || !cookie.value) return null;
-    return cookie.value;
-  };
+  // const getCookieValue = (key: string) => {
+  //   if (!key) return null;
+  //   const cookie = request.cookies.get(key);
+  //   if (!cookie || !cookie.value) return null;
+  //   return cookie.value;
+  // };
 
-  const accessToken = getCookieValue("accessToken");
-  const token = getCookieValue("roleToken");
+  // const accessToken = getCookieValue("accessToken");
+  // const token = getCookieValue("roleToken");
 
-  const pathname = request.nextUrl.pathname;
-  let role: string | undefined = undefined;
+  // const pathname = request.nextUrl.pathname;
+  // let role: string | undefined = undefined;
 
-  if (token && token.length > 0) {
-    try {
-      const { payload } = await jwtVerify(token, secret);
-      role = payload?.role as string;
-    } catch (error) {
-      console.error("JWT verify error:", error);
-      return NextResponse.redirect(new URL("/auth", request.url));
-    }
-  }
+  // if (token && token.length > 0) {
+  //   try {
+  //     const { payload } = await jwtVerify(token, secret);
+  //     role = payload?.role as string;
+  //   } catch (error) {
+  //     console.error("JWT verify error:", error);
+  //     return NextResponse.redirect(new URL("/auth", request.url));
+  //   }
+  // }
 
-  if (pathname.startsWith("/admin") && !accessToken) {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-  if (pathname.startsWith("/admin") && role !== "ADMIN") {
-    return NextResponse.redirect(new URL("/unauthorized", request.url));
-  }
+  // if (pathname.startsWith("/admin") && !accessToken) {
+  //   return NextResponse.redirect(new URL("/auth", request.url));
+  // }
+  // if (pathname.startsWith("/admin") && role !== "ADMIN") {
+  //   return NextResponse.redirect(new URL("/unauthorized", request.url));
+  // }
 
-  if (pathname.startsWith("/freelancer") && !accessToken) {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-  if (pathname.startsWith("/freelancer") && role !== "FREELANCER") {
-    return NextResponse.redirect(new URL("/unauthorized", request.url));
-  }
+  // if (pathname.startsWith("/freelancer") && !accessToken) {
+  //   return NextResponse.redirect(new URL("/auth", request.url));
+  // }
+  // if (pathname.startsWith("/freelancer") && role !== "FREELANCER") {
+  //   return NextResponse.redirect(new URL("/unauthorized", request.url));
+  // }
   
-  if (pathname.startsWith("/owner") && !accessToken) {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-  if (pathname.startsWith("/owner") && role !== "OWNER") {
-    return NextResponse.redirect(new URL("/unauthorized", request.url));
-  }
+  // if (pathname.startsWith("/owner") && !accessToken) {
+  //   return NextResponse.redirect(new URL("/auth", request.url));
+  // }
+  // if (pathname.startsWith("/owner") && role !== "OWNER") {
+  //   return NextResponse.redirect(new URL("/unauthorized", request.url));
+  // }
 
-  if (
-    (pathname.startsWith("/auth") && accessToken) ||
-    (pathname.startsWith("/completeprofile") && accessToken)
-  ) {
-    return NextResponse.redirect(new URL(`${role?.toLocaleLowerCase()}/dashboard`, request.url));
-  }
+  // if (
+  //   (pathname.startsWith("/auth") && accessToken) ||
+  //   (pathname.startsWith("/completeprofile") && accessToken)
+  // ) {
+  //   return NextResponse.redirect(new URL(`${role?.toLocaleLowerCase()}/dashboard`, request.url));
+  // }
 
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/admin/:path*",
-    "/freelancer/:path*",
-    "/owner/:path*",
-    "/auth/:path*",
-    "/completeprofile/:path*",
+    // "/admin/:path*",
+    // "/freelancer/:path*",
+    // "/owner/:path*",
+    // "/auth/:path*",
+    // "/completeprofile/:path*",
   ],
 };
