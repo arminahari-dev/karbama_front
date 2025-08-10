42bd26ec51d598faa150a4541732a5879c6f3c5c
armin ahari <ahariarmin@gmail.com>
2025-08-10 14:07:28 +0330

    fix: fix alignment of logout loading ui

--- src/components/ui/navbar/NavBar.tsx
+++ src/components/ui/navbar/NavBar.tsx
@@ -108,8 +108,8 @@ const NavBar: React.FC = () => {
               className="flex !justify-between !items-center !px-4 !py-2 hover:bg-border rounded-lg text-text transition-colors duration-200 cursor-pointer"
             >
               {isPendingLogOutUser ? (
-                <div className="flex justify-center items-center">
-                  <ArrowPathIcon className="size-6 text-text animate-spin" />
+                <div className="flex justify-center items-center w-full">
+                  <ArrowPathIcon className="size-4 text-text animate-spin" />
                 </div>
               ) : (
                 <>
